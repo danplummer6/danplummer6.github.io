2145cc8894af3c3823b4f5072959421344dacc27
dan plummer <danplumme6@fullsail.edu>
2016-06-28 14:44:59 -0500

    Github.io Portfolio

---
+++ README.md
@@ -0,0 +1,3 @@
+# Daniel Plummer Portfolio
+
+Please feel free to contact me at danplummer6@gmail.com
---
+++ js/scrolling-nav.js
@@ -0,0 +1,21 @@
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 600) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+            // Below changes duration and ease animation
+        }, 1500, 'easeInOutQuint');
+        event.preventDefault();
+    });
+});
